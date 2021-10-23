d297e0336035320223e81ce8245e28442580d2d8
Josh Harris <81477294+JHarrisJoshua@users.noreply.github.com>
2021-10-23 17:43:13 -0400

    commit - update - ready to submit

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,16 +6,118 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    var numberofPhotos: Int!
+    
+    
+    let myRefreshControl = UIRefreshControl()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 400
+        
+        loadPhotos()
+        
+        myRefreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        
     }
     
+    func loadPhotos() {
+        if numberofPhotos == nil {
+            numberofPhotos = 20
+        }
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberofPhotos
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if numberofPhotos == nil {
+            numberofPhotos = 20
+        }
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberofPhotos
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
 
+        
+    }
+    
+    func loadMorePhotos() {
+        
+        numberofPhotos = numberofPhotos + 20
+        loadPhotos()
+        self.tableView.reloadData()
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
+    
+    @objc func onRefresh() {
+        numberofPhotos = 20
+        loadPhotos()
+        self.tableView.reloadData()
+        self.myRefreshControl.endRefreshing()
+    }
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Joshua Harris on 10/23/21.
+//
+
+import UIKit
+
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    
+}
