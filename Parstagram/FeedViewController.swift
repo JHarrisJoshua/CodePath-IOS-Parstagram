eed8c583f9186f32fca843fe69c19aa725400e84
Josh Harris <81477294+JHarrisJoshua@users.noreply.github.com>
2021-10-26 20:15:41 -0400

    commit submit

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -18,6 +18,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var showsCommentBar = false
 
     var posts = [PFObject]()
+    var selectedPost: PFObject!
     var numberofPhotos: Int!
     
     
@@ -102,6 +103,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         // Create the comment
+        let comment = PFObject(className: "Comments")
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()!
+        
+        selectedPost.add(comment, forKey: "comments")
+        
+        selectedPost.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+        tableView.reloadData()
         
         // Clear and dismiss the input bar
         commentBar.inputTextView.text = nil
@@ -225,8 +241,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         if indexPath.row == comments.count + 1 {
             showsCommentBar = true
             becomeFirstResponder()
-            
             commentBar.inputTextView.becomeFirstResponder()
+            selectedPost = post
         }
         
 //        comment["text"] = "This is a random comment"
